cda518124909dd63243a7af548ed792bf84b40bb
Moyster <oysterized@gmail.com>
2017-03-17 12:32:32 +0100

    board: move flags to its own config file (flags.mk)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,16 +13,3 @@ TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
 # Fragments include
 -include $(DEVICE_PATH)/board/*.mk
-
-## Fun with flags
-
-# MTK flags
-BOARD_USES_MTK_HARDWARE := true
-
-# Global flags
-BOARD_GLOBAL_CFLAGS += -DDISABLE_ASHMEM_TRACKING
-BOARD_GLOBAL_CFLAGS += -DCAMERA_VENDOR_L_COMPAT
-
-# Camera
-TARGET_HAS_LEGACY_CAMERA_HAL1 := true
-
