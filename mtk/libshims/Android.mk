609d285ef06f29585c24d96a4933f8b550a9ef8c
Mister Oyster <oysterized@gmail.com>
2017-04-29 21:46:41 +0200

    libshim: cleanup

--- mtk/libshims/Android.mk
+++ mtk/libshims/Android.mk
@@ -28,7 +28,7 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
     ui/mtk_ui.cpp \
-    ui/mtk_gbc1.cpp \
+    ui/mtk_gbc1.cpp
 
 LOCAL_SHARED_LIBRARIES := libbinder libui
 LOCAL_MODULE := libshim_ui
