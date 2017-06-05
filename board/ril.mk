a00523701d61ed3c9e86f0d934cef01b3f89beae
Cesar Matias <cesar.maximo@gmail.com>
2017-06-05 23:10:52 +0100

    update APN & SPN

--- board/ril.mk
+++ board/ril.mk
@@ -4,5 +4,6 @@ BOARD_RIL_CLASS := ../../../$(DEVICE_PATH)/ril
 
 # Telephony
 PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/telephony/apns-conf.xml:system/etc/apns-conf.xml \
     $(DEVICE_PATH)/configs/telephony/ecc_list.xml:system/etc/ecc_list.xml \
     $(DEVICE_PATH)/configs/telephony/spn-conf.xml:system/etc/spn-conf.xml
