79353cd74bbe2a6654883088952d5cadc56ee5f4
Moyster <oysterized@gmail.com>
2017-06-22 10:35:10 +0200

    dt: remove unused rkfill & duplicate fs tools (already in vendor_cm)

--- product/fs.mk
+++ product/fs.mk
@@ -1,18 +1,5 @@
-# Filesystem management tools
+# F2FS
 PRODUCT_PACKAGES += \
     e2fsck \
     fsck.f2fs \
-    mkfs.f2fs \
-    make_ext4fs
-
-# exFAT
-PRODUCT_PACKAGES += \
-    mount.exfat \
-    fsck.exfat \
-    mkfs.exfat
-
-# NTFS
-PRODUCT_PACKAGES += \
-    fsck.ntfs \
-    mkfs.ntfs \
-    mount.ntfs
+    mkfs.f2fs
