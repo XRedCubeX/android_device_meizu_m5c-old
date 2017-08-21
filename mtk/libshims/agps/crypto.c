b9553b9ef0c3106ba21418440e99b97b5bd71e8e
Mister Oyster <oysterized@gmail.com>
2017-08-21 02:42:03 +0200

    libshims: agps: fixes from vendor_mad

--- mtk/libshims/agps/crypto.c
+++ mtk/libshims/agps/crypto.c
@@ -1,11 +1,13 @@
 #include <malloc.h>
 
 void CRYPTO_free(void *str) {
-	    free(str);
+    free(str);
 }
 
 void *CRYPTO_malloc(int num, const char *file, int line) {
-		    return malloc(num);
+    return malloc(num);
 }
 
-void CRYPTO_lock(int mode, int type, const char *file, int line) {}
+void CRYPTO_lock(int mode, int type, const char *file, int line) {
+    CRYPTO_lock(mode, type, file, line);
+}
