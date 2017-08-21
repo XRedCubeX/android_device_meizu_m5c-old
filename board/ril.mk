b952b13430599af0a95338386f118fb68c9d1b29
Mister Oyster <oysterized@gmail.com>
2017-08-21 16:27:13 +0200

    apn: it's in vendor/cm already

--- board/ril.mk
+++ board/ril.mk
@@ -4,6 +4,5 @@ BOARD_RIL_CLASS := ../../../$(DEVICE_PATH)/ril
 
 # Telephony
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/telephony/apns-conf.xml:system/etc/apns-conf.xml \
     $(DEVICE_PATH)/configs/telephony/ecc_list.xml:system/etc/ecc_list.xml \
     $(DEVICE_PATH)/configs/telephony/spn-conf.xml:system/etc/spn-conf.xml
