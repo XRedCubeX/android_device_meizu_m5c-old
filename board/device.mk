fe89be53c77a318610f224487f85c76b8910ce7f
Mister Oyster <oysterized@gmail.com>
2017-09-22 15:52:22 +0200

    healthd: add backlight path

--- board/device.mk
+++ board/device.mk
@@ -7,3 +7,6 @@ BOARD_USES_MTK_HARDWARE := true
 # Camera flags
 BOARD_GLOBAL_CFLAGS += -DCAMERA_VENDOR_L_COMPAT
 TARGET_CAMERASERVICE_CLOSES_NATIVE_HANDLES := true
+
+# Charger
+BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
