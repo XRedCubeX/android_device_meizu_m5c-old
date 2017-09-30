da0a2cd07be071aadf525bf87bd6890c91f147b1
Mister Oyster <oysterized@gmail.com>
2017-09-30 14:12:44 +0200

    exfat: build with kernel driver support

--- board/device.mk
+++ board/device.mk
@@ -10,3 +10,6 @@ TARGET_CAMERASERVICE_CLOSES_NATIVE_HANDLES := true
 
 # Charger
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
+
+# Extended Filesystem Support
+TARGET_KERNEL_HAVE_EXFAT := true
