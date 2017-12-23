54f61484388dbf8078814039b537a539caaacb0a
Mister Oyster <oysterized@gmail.com>
2017-12-23 15:16:57 +0100

    configs: audio: convert audio_effects.conf to .xml

--- board/audio.mk
+++ board/audio.mk
@@ -2,7 +2,7 @@
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(DEVICE_PATH)/configs/audio/audio_device.xml:system/etc/audio_device.xml \
-    $(DEVICE_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf
+    $(DEVICE_PATH)/configs/audio/audio_effects.xml:system/etc/audio_effects.xml
 
 # Media config
 PRODUCT_COPY_FILES += \
