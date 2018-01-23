a2386698bc721549621ee2246d8bf77c7958b3ab
Moyster <oysterized@gmail.com>
2018-01-23 16:21:29 +0100

    connectivity: add more wifi props

--- board/conn.mk
+++ board/conn.mk
@@ -24,15 +24,20 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     $(DEVICE_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_mt66xx
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_mt66xx
-WIFI_DRIVER_FW_PATH_PARAM:="/dev/wmtWifi"
-WIFI_DRIVER_FW_PATH_STA:=STA
-WIFI_DRIVER_FW_PATH_AP:=AP
-WIFI_DRIVER_FW_PATH_P2P:=P2P
+
+WPA_SUPPLICANT_VERSION 			:= VER_0_8_X
+BOARD_HOSTAPD_DRIVER 			:= NL80211
+BOARD_HOSTAPD_PRIVATE_LIB		:= lib_driver_cmd_mt66xx
+BOARD_WLAN_DEVICE			:= MediaTek
+BOARD_WPA_SUPPLICANT_DRIVER		:= NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB	:= lib_driver_cmd_mt66xx
+WIFI_DRIVER_FW_PATH_PARAM		:= "/dev/wmtWifi"
+WIFI_DRIVER_FW_PATH_STA			:= STA
+WIFI_DRIVER_FW_PATH_AP			:= AP
+WIFI_DRIVER_FW_PATH_P2P			:= P2P
+WIFI_DRIVER_STATE_CTRL_PARAM		:= "/dev/wmtWifi"
+WIFI_DRIVER_STATE_ON		 	:= 1
+WIFI_DRIVER_STATE_OFF		 	:= 0
 
 # Tethering
 PRODUCT_PROPERTY_OVERRIDES += net.tethering.noprovisioning=true
