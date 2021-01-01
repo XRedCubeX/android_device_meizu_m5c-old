29ac05ee48242e3df3e6e52769fb071b44a138dd
XRed_CubeX <xredcubex@gmail.com>
2021-01-01 23:24:20 +0100

    M5c:Remove Recovery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,6 @@ TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
 # Device cfg
 -include $(DEVICE_PATH)/PlatformConfig.mk
--include $(DEVICE_PATH)/RecoveryConfig.mk
 
 # Fragments include
 -include $(DEVICE_PATH)/board/*.mk
