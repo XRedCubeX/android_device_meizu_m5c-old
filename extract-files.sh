6e215106d5636f859539a6817c773da058561579
XRed_CubeX <xredcubex@gmail.com>
2021-01-03 22:42:58 +0100

    M5c:Change extract file script

--- extract-files.sh
+++ extract-files.sh
@@ -57,4 +57,4 @@ rm -rf $BASE/*
 #extract ../../$VENDOR/$DEVICE/proprietary-files-mtk.txt $BASE
 extract ../../$VENDOR/$DEVICE/proprietary-files.txt $BASE
 
-./setup-makefiles.sh
+#./setup-makefiles.sh
