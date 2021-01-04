9d5c23fd9e727c014061dba156ec87fa40efff10
XRed_CubeX <xredcubex@gmail.com>
2021-01-04 15:46:54 +0100

    Some fixes

---
+++ board/ramdisk.mk
@@ -0,0 +1,15 @@
+# device rootdir
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
+    $(DEVICE_PATH)/rootdir/init.mt6735.rc:root/init.mt6735.rc \
+    $(DEVICE_PATH)/rootdir/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
+    $(DEVICE_PATH)/rootdir/ueventd.mt6735.rc:root/ueventd.mt6735.rc \
+    $(DEVICE_PATH)/rootdir/init.recovery.mt6735.rc:root/init.recovery.mt6735.rc \
+    $(DEVICE_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
+    $(DEVICE_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
+    $(DEVICE_PATH)/rootdir/factory_init.usb.rc:root/factory_init.usb.rc \
+    $(DEVICE_PATH)/rootdir/fstab.mt6735:root/fstab.mt6735 \
+    $(DEVICE_PATH)/rootdir/init.project.rc:root/init.project.rc \
+    $(DEVICE_PATH)/rootdir/init.rc:root/init.rc \
+    $(DEVICE_PATH)/rootdir/recovery.fstab:root/recovery.fstab \
+    $(DEVICE_PATH)/rootdir/ueventd.rc:root/ueventd.rc
