7ac73bf0e1d0b3cb7e8bceaf63f25ae6f9aaf570
XRedCubeX <xredcubex@hotmail.com>
2021-01-09 10:01:23 +0100

    Fixes some symbols

---
+++ mtk/libmtk_symbols/mtk_gui.cpp
@@ -0,0 +1,26 @@
+#include <stdint.h>
+
+extern "C" {
+    void _ZN7android14SurfaceControl8setLayerEj(uint32_t);
+
+    void _ZN7android14SurfaceControl8setLayerEi(int32_t layer){
+        _ZN7android14SurfaceControl8setLayerEj(static_cast<uint32_t>(layer));
+    void _ZN7android11IDumpTunnel11asInterfaceERKNS_2spINS_7IBinderEEE();
+ void _ZN7android9CallStackC1EPKci(char const*, int);
+ void _ZN7android9CallStack6updateEii(int, int);
+
+ void _ZN7android9CallStackC1EPKcii(char const* logtag, int ignoreDepth, int maxDepth){
+  maxDepth = maxDepth-1;
+  maxDepth = maxDepth+1;
+  _ZN7android9CallStackC1EPKci(logtag, ignoreDepth);
+  
+ }
+
+ void _ZN7android9CallStack6updateEiii(int ignoreDepth, int maxDepth, int tid){
+  maxDepth = maxDepth-1;
+  maxDepth = maxDepth+1; 
+  _ZN7android9CallStack6updateEii(ignoreDepth, tid);
+ }
+}
+    }
+}
--- mtk/libshims/gui/mtk_gui.cpp
+++ mtk/libshims/gui/mtk_gui.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <ui/GraphicBufferMapper.h>
 #include <ui/Rect.h>
 
@@ -6,5 +7,12 @@ extern "C" {
 
     void _ZN7android19GraphicBufferMapper4lockEPK13native_handleiRKNS_4RectEPPv(buffer_handle_t handle, int usage, const android::Rect& bounds, void** vaddr) {
         _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(handle, static_cast<uint32_t>(usage), bounds, vaddr);
-    }
+ void _ZN7android11IDumpTunnel11asInterfaceERKNS_2spINS_7IBinderEEE();
 }
+    }
+
+/*
+extern "C" {
+ void _ZN7android11IDumpTunnel11asInterfaceERKNS_2spINS_7IBinderEEE(const android::sp<android::IBinder>&) {}
+}
+*/
